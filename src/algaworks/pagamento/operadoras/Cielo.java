4983062acb8a66a6abcd1a3d0af88eeb026c54aa
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:32:39 -0300

    Update Cielo.java

--- src/algaworks/pagamento/operadoras/Cielo.java
+++ src/algaworks/pagamento/operadoras/Cielo.java
@@ -5,6 +5,7 @@ import algaworks.pagamento.Cartao;
 import algaworks.pagamento.Operadora;
 
 public class Cielo implements Operadora {
+    
     @Override
     public boolean autorizar(Autorizavel autorizavel, Cartao cartao) {
         return cartao.getNumeroCartao().startsWith("123") && autorizavel.getValorTotal() < 100;
