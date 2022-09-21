3428133d21f26fee6223d752961345d7fd0f1aff
Taís Bueno Vidotto <100791472+Triliam@users.noreply.github.com>
2022-09-21 11:30:42 -0300

    Update ImpressoraHP.java

--- src/algaworks/impressao/impressoras/ImpressoraHP.java
+++ src/algaworks/impressao/impressoras/ImpressoraHP.java
@@ -4,6 +4,7 @@ import algaworks.impressao.Impressora;
 import algaworks.impressao.Imprimivel;
 
 public class ImpressoraHP implements Impressora {
+    
     @Override
     public void imprimir(Imprimivel imprimivel) {
         System.out.println("impressao feita na HP");
